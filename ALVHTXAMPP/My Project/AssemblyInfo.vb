75b7c38618726992870f017d677b9a3d6bf42a6f
Daniel S. Billing <daniel@kilobyte.no>
2022-04-18 21:27:07 +0200

    Added signing

--- ALVHTXAMPP/My Project/AssemblyInfo.vb
+++ ALVHTXAMPP/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.2.2.223")>
-<Assembly: AssemblyFileVersion("0.2.2.223")>
+<Assembly: AssemblyVersion("0.2.2.224")>
+<Assembly: AssemblyFileVersion("0.2.2.224")>
